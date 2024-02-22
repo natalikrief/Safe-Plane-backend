bee52d5a6f5e988811e0fdaa3f83c404ecefb7da
natalikrief <nataliekrief@gmail.com>
2024-02-22 13:46:33 +0200

    My first commit

--- main/urls.py
+++ main/urls.py
@@ -19,10 +19,11 @@ from django.urls import path
 
 # import hello.views
 
-# urlpatterns = [
-#     path("", hello.views.index, name="index"),
-#     path("db/", hello.views.db, name="db"),
-#     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
-#     # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/
-#     # path("admin/", admin.site.urls),
-# ]
+urlpatterns = [
+    # path("", hello.views.index, name="index"),
+    # path("db/", hello.views.db, name="db"),
+
+    # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
+    # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/
+    # path("admin/", admin.site.urls),
+]
